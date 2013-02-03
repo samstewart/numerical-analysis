974dfa6fa1691ceec711258c9e8d7ae8f8a508b5
Sam Stewart <sam@samstewartapps.com>
2013-02-03 10:30:49 -0800

    [fix] a few minor errors and [refactor] the code

--- assignment_1/newtons_method.c
+++ assignment_1/newtons_method.c
@@ -1,45 +1,72 @@
 #include <stdio.h>
 #include <math.h>
-#include <malloc.h>
+#include <stdlib.h>
 	
 double derivative(double x, double *coeff, int n);
 double function(double x, double *coeff, int n);
+void parseCoefficients(double *coeff, int n);
+double runNewtonsMethod(double *coeff, int n, int iterations, double guess);
 
 int main(int argv, const char* argc[]) {
 	
+    printf("Enter the number of coefficients followed by the coefficients themselves: \n");
+    
 	// read in the number of coefficients
 	int n;
 
 	scanf("%d", &n);
 
 	// create a new array to hold the values
-	double *coeff = (double *)malloc(sizeof(double) * n);
+	double *coeff = (double *) malloc(sizeof(double) * n);
 
-	// now read in coefficients
-	int i;
+	// now read in individual coefficients
+    parseCoefficients(coeff, n);
+
+    // number of iterations for newton's method
+    const int iterations = 30;
+    const double guess = 1.5;
+    
+    // no perform newtons method
+    double solution = runNewtonsMethod(coeff, n, iterations, guess);
+
+	free(coeff);
+
+	printf("Approximate solution: %lf\n", solution);
+	
+    return 0;
+}
+
+/** 
+* parses the coefficents from STDIN
+*/
+void parseCoefficients(double *coeff, int n) {
+    int i;
 	
 	for (i = n - 1; i >= 0; i--) {
 		scanf("%lf", &coeff[i]);
 	}
-
-	i = 0;
-
-	double cur_guess = 0;
-	double prev_guess = 2.0;
-
-	int num_iterations = 100;
-
-	for (i = 0; i < num_iterations; i++) {
-		cur_guess  = prev_guess - function(prev_guess, coeff, n) / derivative(prev_guess, coeff, n);
-		prev_guess = cur_guess;
-
-	}
-
-	free(coeff);
-
-	printf("Final solution: %lf\n", cur_guess);
 }
 
+/**
+* Runs newtons method for a given number of iterations on the given 
+* polynomial
+*/
+double runNewtonsMethod(double *coeff, int n, int iterations, double guess) {
+    
+    double cur_guess = guess;
+    
+    int i;
+	for (i = 0; i < iterations; i++) {
+	    // simple newtons method solver
+		cur_guess  = cur_guess - function(cur_guess, coeff, n) / derivative(cur_guess, coeff, n);
+	}
+	
+    return cur_guess;
+}
+
+/**
+* the derivative of the mathematical equation that we wish to solve.
+*/
 double derivative(double x, double *coeff, int n) {
 	// return derivatives
 	int i = 0;
@@ -47,12 +74,15 @@ double derivative(double x, double *coeff, int n) {
 
 	// because its the derivative we skip the first constant
 	for (i = 1; i < n; i++ ) {
-		result += coeff[i] * pow(x, i - 1);
+		result += i * coeff[i] * pow(x, i - 1);
 	}
 
 	return result;
 }
 
+/**
+* mathematical equation that we wish to solve.
+*/
 double function(double x, double *coeff, int n) {
 	// loop through coefficients
 	int i = 0;
